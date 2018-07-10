37b18542064c3bbf5f3adffb4bd7f72d988c57d2
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-07-10 15:51:43 +0200

    Fix startpoint_secure

--- lib/startpoint_secure.lua
+++ lib/startpoint_secure.lua
@@ -7,7 +7,7 @@ function Startpoint_secure.startpoint_secure(profile,way,result,data)
   local highway = way:get_value_by_key("highway")
   local tunnel = way:get_value_by_key("tunnel")
 
-  if highway ~= "motorway" and (not tunnel or tunnel == "") then
+  if highway ~= "motorway" and highway ~= "motorway_link" and (not tunnel or tunnel == "") then
     WayHandlers.startpoint(way,result,data,profile)
   else
     result.is_startpoint = false
